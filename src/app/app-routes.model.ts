025c9564ee80411d0b4e9e3a47e837a94d4b203d
nicoll <lnicolovici@gmail.com>
2020-06-16 11:42:58 +0300

    implemented lazy loading

--- src/app/app-routes.model.ts
+++ src/app/app-routes.model.ts
@@ -1,13 +1,30 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { AuthComponent } from './auth/auth.component';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/auth', pathMatch: 'full' },
+  {
+    path: 'recipes',
+    loadChildren: () =>
+      import('./recipes/recipes.module').then((module) => module.RecipesModule),
+  },
+  {
+    path: 'shopping-list',
+    loadChildren: () =>
+      import('./shopping-list/shopping-list.module').then(
+        (module) => module.ShoppingModule
+      ),
+  },
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./auth/auth.module').then((module) => module.AuthModule),
+  },
 ];
 @NgModule({
-  imports: [RouterModule.forRoot(appRoutes)],
+  imports: [
+    RouterModule.forRoot(appRoutes, { preloadingStrategy: PreloadAllModules }),
+  ],
   exports: [RouterModule],
 })
 export class AppRoutesComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,10 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutesComponent } from './app-routes.model';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { AuthComponent } from './auth/auth.component';
-import { RecipesModule } from './recipes/recipes.module';
-import { ShoppingModule } from './shopping-list/shopping-list.module';
+import { HttpClientModule } from '@angular/common/http';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
-import { AuthModule } from './auth/auth.module';
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
   imports: [
@@ -19,11 +14,8 @@ import { AuthModule } from './auth/auth.module';
     NgbModule,
     AppRoutesComponent,
     HttpClientModule,
-    RecipesModule,
-    ShoppingModule,
     SharedModule,
     CoreModule,
-    AuthModule,
   ],
 
   bootstrap: [AppComponent],
